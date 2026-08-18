c7bf635f831731909a6cf5b3536e3931890b2f9a
Tatsuya Miyagawa <zgmf326@gmail.com>
2026-08-18 22:47:47 +0900

    add: SQL Serverの基本操作を追加

--- day1/test.sql
+++ day1/test.sql
@@ -1,1 +1,24 @@
 SELECT @@VERSION;
+
+--Cmd + Shift + Eで実行する！！
+CREATE DATABASE sql_study;
+
+USE sql_study;
+
+--コンマの位置に注意。
+CREATE TABLE users(
+  id INT PRIMARY KEY,
+  name NVARCHAR(50),
+  age INT,
+  city NVARCHAR(50)
+);
+
+--Nを忘れない。
+INSERT into users(id,name,age,city)
+VALUES
+  (1, N'山田', 25, N'福井'),
+  (2, N'田中', 20, N'福井'),
+  (3, N'佐藤', 30, N'福井');
+
+--*ですべて表示
+SELECT * FROM users;

e364349a7865384581eeeb1d9d14b3aa093b61fb
Tatsuya Miyagawa <zgmf326@gmail.com>
2026-09-10 09:30:30 +0900

    add:AVGとINの練習

---
+++ subquery.sql
@@ -0,0 +1,72 @@
+USE sql_study;
+
+INSERT INTO users (id,name, age, city)
+VALUES
+(6, N'高橋', 18, N'福井'),
+(7, N'伊藤', 40, N'福井'),
+(8, N'中村', 22, N'大阪'),
+(9, N'加藤', 35, N'東京'),
+(10, N'吉田', 50, N'福井');
+
+SELECT *
+FROM users;
+
+--サブクエリとはSQLの中に、別のSQLを入れること
+-- 比較するための値を、別のSELECTで作ってしまう
+
+-- 練習問題
+-- users全体の平均年齢より年齢が高いユーザーを表示する
+SELECT
+  users.name,
+  users.age
+FROM users
+WHERE users.age > (
+  SELECT AVG(age)
+  FROM users
+);
+
+-- users全体の平均年齢より年齢が高いユーザーの、名前と年齢を表示
+SELECT
+  users.name,
+  users.age
+FROM users
+WHERE users.age >(
+  SELECT AVG(age)
+  FROM users
+);
+
+-- users テーブルから、福井県のユーザーの平均年齢より年齢が高いユーザーの「名前・年齢・都道府県」を表示
+SELECT
+  users.name,
+  users.age,
+  users.city
+FROM users
+WHERE users.age >(
+  SELECT AVG(age)
+  FROM users
+  WHERE users.city =N'福井'
+);
+
+-- 大阪のユーザーの平均年齢より年齢が高いユーザーを、全ユーザーの中から取得
+-- 表示するのはid、name、age、city
+SELECT
+  users.id,
+  users.name,
+  users.age,
+  users.city
+FROM users
+WHERE users.age >(
+  SELECT AVG(age)
+  FROM users
+  WHERE users.city =N'大阪'
+);
+
+-- 「福井」に住んでいるユーザーと同じ年齢のユーザーを、全ユーザーから取得してください。
+-- 表示するのはid、name、age、city
+SELECT *
+FROM users
+WHERE users.age IN(
+  SELECT users.age
+  FROM users
+  WHERE users.city = N'福井'
+);
